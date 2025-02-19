78ca03420aae0514a3638ef6ecf3c48f8e1f1f3a
lakshmi sinil <lakshmisinil2002@gmail.com>
2025-02-19 19:38:56 +0530

    removed usercontroller,add nodemon ,updated packagejson

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../services/userService');
 
 const router = express.Router();
 
